d3ca69f790fcf10074b146448124acacc3957999
maneroto <maneroto@hotmail.com>
2022-10-11 12:47:07 -0500

    Add BottomNavigation widget and provider

--- mobile-app/app/lib/main.dart
+++ mobile-app/app/lib/main.dart
@@ -1,59 +1,66 @@
+import 'package:app/providers/PageProvider.dart';
 import 'package:app/widgets/atoms/Button.dart';
 import 'package:app/widgets/molecules/IconButton.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import "./widgets/atoms/Typography.dart";
+import './widgets/organisms/BottomNavigation.dart';
 import "./styles/colors.dart" as app_colors;
 import "./styles/icons.dart" as app_icons;
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    color: Colors.white,
-    home: Scaffold(
-      body: SafeArea(
-          child: Column(
-        children: <Widget>[
-          AppTypography(
-            align: TextAlign.left,
-            type: "h1",
-            text: "This is an h1",
-            color: app_colors.primary,
-          ),
-          AppTypography(
-            align: TextAlign.left,
-            type: "h2",
-            text: "This is an h2",
-            color: app_colors.primary,
-          ),
-          AppTypography(
-            align: TextAlign.left,
-            type: "h3",
-            text: "This is an h3",
-            color: app_colors.primary,
-          ),
-          AppTypography(
-            align: TextAlign.left,
-            type: "subtitle",
-            text: "This is a subtitle",
-            color: app_colors.primary,
-          ),
-          AppTypography(
-            align: TextAlign.left,
-            type: "body1",
-            text: "This is a body1",
-            color: app_colors.primary,
-          ),
-          AppTypography(
-            align: TextAlign.left,
-            type: "body2",
-            text: "This is a body2",
-            color: app_colors.primary,
-          ),
-          AppButton(text: "Click here", onPressed: () => {}),
-          AppIconButton(
-              text: "Some text", icon: app_icons.sellCar, onPressed: () => {})
-        ],
-      )),
+  runApp(ChangeNotifierProvider(
+    create: (context) => PageProvider(),
+    child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+      color: Colors.white,
+      home: Scaffold(
+        body: SafeArea(
+            child: Column(
+          children: <Widget>[
+            AppTypography(
+              align: TextAlign.left,
+              type: "h1",
+              text: "This is an h1",
+              color: app_colors.primary,
+            ),
+            AppTypography(
+              align: TextAlign.left,
+              type: "h2",
+              text: "This is an h2",
+              color: app_colors.primary,
+            ),
+            AppTypography(
+              align: TextAlign.left,
+              type: "h3",
+              text: "This is an h3",
+              color: app_colors.primary,
+            ),
+            AppTypography(
+              align: TextAlign.left,
+              type: "subtitle",
+              text: "This is a subtitle",
+              color: app_colors.primary,
+            ),
+            AppTypography(
+              align: TextAlign.left,
+              type: "body1",
+              text: "This is a body1",
+              color: app_colors.primary,
+            ),
+            AppTypography(
+              align: TextAlign.left,
+              type: "body2",
+              text: "This is a body2",
+              color: app_colors.primary,
+            ),
+            AppButton(text: "Click here", onPressed: () => {}),
+            AppIconButton(
+                text: "Some text", icon: app_icons.sellCar, onPressed: () => {})
+          ],
+        )),
+        bottomNavigationBar: const AppBottomNavigation(),
+      ),
     ),
   ));
 }
