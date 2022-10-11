11b1b562c9090d1de14efe66f5331aae3b826b6a
Giovanni Finocchiaro <A01704007@tec.mx>
2022-10-11 12:33:13 -0500

    const in main.dart

--- mobile-app/app/lib/main.dart
+++ mobile-app/app/lib/main.dart
@@ -13,31 +13,31 @@ void main() {
       body: SafeArea(
           child: Column(
         children: <Widget>[
-          AppTypography(
+          const AppTypography(
             align: TextAlign.left,
             type: "h1",
             text: "This is an h1",
             color: app_colors.primary,
           ),
-          AppTypography(
+          const AppTypography(
             align: TextAlign.left,
             type: "h2",
             text: "This is an h2",
             color: app_colors.primary,
           ),
-          AppTypography(
+          const AppTypography(
             align: TextAlign.left,
             type: "h3",
             text: "This is an h3",
             color: app_colors.primary,
           ),
-          AppTypography(
+          const AppTypography(
             align: TextAlign.left,
             type: "subtitle",
             text: "This is a subtitle",
             color: app_colors.primary,
           ),
-          AppTypography(
+          const AppTypography(
             align: TextAlign.left,
             type: "body1",
             text: "This is a body1",
