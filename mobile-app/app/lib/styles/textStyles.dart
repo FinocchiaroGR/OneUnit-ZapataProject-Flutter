d8fe91f2843d509c36fab949c202b96a5cb428ef
maneroto <maneroto@hotmail.com>
2022-10-10 23:02:28 -0500

    Add Typography Widget

--- mobile-app/app/lib/styles/textStyles.dart
+++ mobile-app/app/lib/styles/textStyles.dart
@@ -7,40 +7,34 @@ const h1 = TextStyle(
   fontSize: 52,
   fontFamily: 'Browallia',
   fontWeight: FontWeight.w700,
-  color: appColors.primary,
 );
 
 const h2 = TextStyle(
   fontSize: 44,
   fontFamily: 'Browallia',
   fontWeight: FontWeight.w700,
-  color: appColors.primary,
 );
 
 const h3 = TextStyle(
   fontSize: 38,
   fontFamily: 'Browallia',
   fontWeight: FontWeight.w700,
-  color: appColors.primary,
 );
 
 const subtitle = TextStyle(
   fontSize: 28,
   fontFamily: 'Helvetica',
-  color: appColors.primary,
   height: 1.2,
 );
 
 const body1 = TextStyle(
   fontSize: 20,
   fontFamily: 'Helvetica',
-  color: appColors.primary,
   height: 1.4,
 );
 
 const body2 = TextStyle(
   fontSize: 16,
   fontFamily: 'Helvetica',
-  color: appColors.primary,
   height: 1.3,
 );
