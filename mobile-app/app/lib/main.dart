df165d0f5cc284c0b9b63a67f96a4ac288fd7032
maneroto <maneroto@hotmail.com>
2022-10-11 01:20:31 -0500

    Add atom widgets to main

--- mobile-app/app/lib/main.dart
+++ mobile-app/app/lib/main.dart
@@ -3,18 +3,51 @@ import "./widgets/atoms/Typography.dart";
 import "./styles/colors.dart" as app_colors;
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     color: Colors.white,
     home: Scaffold(
       body: SafeArea(
-        child: AppTypography(
-          align: TextAlign.left,
-          type: "h2",
-          text: "hello world",
-          color: app_colors.secondary,
-        ),
-      ),
+          child: Wrap(
+        children: const <AppTypography>[
+          AppTypography(
+            align: TextAlign.left,
+            type: "h1",
+            text: "This is an h1",
+            color: app_colors.primary,
+          ),
+          AppTypography(
+            align: TextAlign.left,
+            type: "h2",
+            text: "This is an h2",
+            color: app_colors.primary,
+          ),
+          AppTypography(
+            align: TextAlign.left,
+            type: "h3",
+            text: "This is an h3",
+            color: app_colors.primary,
+          ),
+          AppTypography(
+            align: TextAlign.left,
+            type: "subtitle",
+            text: "This is a subtitle",
+            color: app_colors.primary,
+          ),
+          AppTypography(
+            align: TextAlign.left,
+            type: "body1",
+            text: "This is a body1",
+            color: app_colors.primary,
+          ),
+          AppTypography(
+            align: TextAlign.left,
+            type: "body2",
+            text: "This is a body2",
+            color: app_colors.primary,
+          ),
+        ],
+      )),
     ),
   ));
 }
