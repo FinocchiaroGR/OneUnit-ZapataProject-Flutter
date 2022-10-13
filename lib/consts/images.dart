3ce783f6eb5a2e4bc28494b3caf73c3bf1c6a2e6
maneroto <maneroto@hotmail.com>
2022-10-13 16:25:53 -0500

    Add Carousel organism, Add scrollable view to main

--- lib/consts/images.dart
+++ lib/consts/images.dart
@@ -1,1 +1,2 @@
 const logo = "assets/images/logo.png";
+const carPlaceholder = "assets/images/car-placeholder.jpg";
--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,29 @@ import 'package:provider/provider.dart';
 import "./widgets/atoms/Typography.dart";
 import 'package:app/widgets/organisms/AppBar.dart';
 import './widgets/organisms/BottomNavigation.dart';
+import "package:app/widgets/organisms/Carousel.dart";
 import "./styles/colors.dart" as app_colors;
 import "./styles/icons.dart" as app_icons;
+import "package:app/consts/images.dart" as app_images;
+
+const dummyCarSlides = [
+  {
+    'src': app_images.logo,
+    'name': 'OneUnit',
+  },
+  {
+    'src': app_images.carPlaceholder,
+    'name': 'Zapata',
+  },
+  {
+    'src': app_images.carPlaceholder,
+    'name': 'Connect',
+  },
+  {
+    'src': app_images.carPlaceholder,
+    'name': 'For clients',
+  },
+];
 
 void main() {
   runApp(ChangeNotifierProvider(
@@ -16,53 +37,59 @@ void main() {
       debugShowCheckedModeBanner: false,
       color: Colors.white,
       home: Scaffold(
-        appBar: const AppAppBar(title: "LamborghiniP"),
-        body: SafeArea(
-            child: Column(
-          children: <Widget>[
-            const AppTypography(
-              align: TextAlign.left,
-              type: "h1",
-              text: "This is an h1",
-              color: app_colors.primary,
+          appBar: const AppAppBar(title: "LamborghiniP"),
+          bottomNavigationBar: const AppBottomNavigation(),
+          body: SingleChildScrollView(
+            child: SafeArea(
+              child: Column(
+                children: <Widget>[
+                  const AppCarousel(items: dummyCarSlides),
+                  const AppTypography(
+                    align: TextAlign.left,
+                    type: "h1",
+                    text: "This is an h1",
+                    color: app_colors.primary,
+                  ),
+                  const AppTypography(
+                    align: TextAlign.left,
+                    type: "h2",
+                    text: "This is an h2",
+                    color: app_colors.primary,
+                  ),
+                  const AppTypography(
+                    align: TextAlign.left,
+                    type: "h3",
+                    text: "This is an h3",
+                    color: app_colors.primary,
+                  ),
+                  const AppTypography(
+                    align: TextAlign.left,
+                    type: "subtitle",
+                    text: "This is a subtitle",
+                    color: app_colors.primary,
+                  ),
+                  const AppTypography(
+                    align: TextAlign.left,
+                    type: "body1",
+                    text: "This is a body1",
+                    color: app_colors.primary,
+                  ),
+                  const AppTypography(
+                    align: TextAlign.left,
+                    type: "body2",
+                    text: "This is a body2",
+                    color: app_colors.primary,
+                  ),
+                  AppButton(text: "Click here", onPressed: () => {}),
+                  AppIconButton(
+                    text: "Some text",
+                    icon: app_icons.sellCar,
+                    onPressed: () => {},
+                  ),
+                ],
+              ),
             ),
-            const AppTypography(
-              align: TextAlign.left,
-              type: "h2",
-              text: "This is an h2",
-              color: app_colors.primary,
-            ),
-            const AppTypography(
-              align: TextAlign.left,
-              type: "h3",
-              text: "This is an h3",
-              color: app_colors.primary,
-            ),
-            const AppTypography(
-              align: TextAlign.left,
-              type: "subtitle",
-              text: "This is a subtitle",
-              color: app_colors.primary,
-            ),
-            const AppTypography(
-              align: TextAlign.left,
-              type: "body1",
-              text: "This is a body1",
-              color: app_colors.primary,
-            ),
-            const AppTypography(
-              align: TextAlign.left,
-              type: "body2",
-              text: "This is a body2",
-              color: app_colors.primary,
-            ),
-            AppButton(text: "Click here", onPressed: () => {}),
-            AppIconButton(
-                text: "Some text", icon: app_icons.sellCar, onPressed: () => {})
-          ],
-        )),
-        bottomNavigationBar: const AppBottomNavigation(),
-      ),
+          )),
     ),
   ));
 }
