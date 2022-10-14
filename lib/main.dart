b10190a8f22a344cd2fbf165b9b1c3b0bae3810d
maneroto <maneroto@hotmail.com>
2022-10-14 10:46:17 -0500

    Add routing system

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,10 @@
-import 'package:app/providers/PageProvider.dart';
-import 'package:app/widgets/atoms/Button.dart';
-import 'package:app/widgets/molecules/IconButton.dart';
 import 'package:flutter/material.dart';
+import 'package:app/providers/PageProvider.dart';
 import 'package:provider/provider.dart';
-import "./widgets/atoms/Typography.dart";
-import 'package:app/widgets/organisms/AppBar.dart';
-import './widgets/organisms/BottomNavigation.dart';
-import "package:app/widgets/organisms/Carousel.dart";
-import "./styles/colors.dart" as app_colors;
-import "./styles/icons.dart" as app_icons;
-import "package:app/consts/images.dart" as app_images;
 
-const dummyCarSlides = [
-  {
-    'src': app_images.logo,
-    'name': 'OneUnit',
-  },
-  {
-    'src': app_images.carPlaceholder,
-    'name': 'Zapata',
-  },
-  {
-    'src': app_images.carPlaceholder,
-    'name': 'Connect',
-  },
-  {
-    'src': app_images.carPlaceholder,
-    'name': 'For clients',
-  },
-];
+import 'package:app/router.dart' as app_router;
+
+import 'package:app/consts/urls.dart' as app_urls;
 
 void main() {
   runApp(ChangeNotifierProvider(
@@ -36,60 +12,8 @@ void main() {
     child: MaterialApp(
       debugShowCheckedModeBanner: false,
       color: Colors.white,
-      home: Scaffold(
-          appBar: const AppAppBar(title: "LamborghiniP"),
-          bottomNavigationBar: const AppBottomNavigation(),
-          body: SingleChildScrollView(
-            child: SafeArea(
-              child: Column(
-                children: <Widget>[
-                  const AppCarousel(items: dummyCarSlides),
-                  const AppTypography(
-                    align: TextAlign.left,
-                    type: "h1",
-                    text: "This is an h1",
-                    color: app_colors.primary,
-                  ),
-                  const AppTypography(
-                    align: TextAlign.left,
-                    type: "h2",
-                    text: "This is an h2",
-                    color: app_colors.primary,
-                  ),
-                  const AppTypography(
-                    align: TextAlign.left,
-                    type: "h3",
-                    text: "This is an h3",
-                    color: app_colors.primary,
-                  ),
-                  const AppTypography(
-                    align: TextAlign.left,
-                    type: "subtitle",
-                    text: "This is a subtitle",
-                    color: app_colors.primary,
-                  ),
-                  const AppTypography(
-                    align: TextAlign.left,
-                    type: "body1",
-                    text: "This is a body1",
-                    color: app_colors.primary,
-                  ),
-                  const AppTypography(
-                    align: TextAlign.left,
-                    type: "body2",
-                    text: "This is a body2",
-                    color: app_colors.primary,
-                  ),
-                  AppButton(text: "Click here", onPressed: () => {}),
-                  AppIconButton(
-                    text: "Some text",
-                    icon: app_icons.sellCar,
-                    onPressed: () => {},
-                  ),
-                ],
-              ),
-            ),
-          )),
+      initialRoute: app_urls.home,
+      routes: app_router.routes,
     ),
   ));
 }
--- lib/router.dart
+++ lib/router.dart
@@ -1,1 +1,9 @@
+import 'package:app/consts/urls.dart' as app_urls;
 
+import 'package:app/pages/ComponentsShowcase.dart';
+import 'package:app/pages/Dashboard.dart';
+
+final routes = {
+  app_urls.home: (ctx) => ComponentsShowcase(),
+  app_urls.componentsShowcase: (ctx) => const Dashboard(),
+};
