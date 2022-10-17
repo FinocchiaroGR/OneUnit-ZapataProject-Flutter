1cabccf731790092fe787a451a8d02bcf3ef81dd
maneroto <maneroto@hotmail.com>
2022-10-17 10:53:19 -0500

    Create map widget

--- lib/router.dart
+++ lib/router.dart
@@ -2,8 +2,11 @@ import 'package:app/consts/urls.dart' as app_urls;
 
 import 'package:app/pages/ComponentsShowcase.dart';
 import 'package:app/pages/Dashboard.dart';
+import 'package:flutter/cupertino.dart';
 
-final routes = {
-  app_urls.home: (ctx) => ComponentsShowcase(),
-  app_urls.componentsShowcase: (ctx) => const Dashboard(),
+import 'package:app/widgets/atoms/LocationMap.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  app_urls.home: (ctx) => const Dashboard(),
+  app_urls.componentsShowcase: (ctx) => ComponentsShowcase(),
 };
