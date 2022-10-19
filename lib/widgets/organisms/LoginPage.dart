4d5f975499f52e6eeaabcf04e02d37f1ad8111cb
feeramezquita <a01421954@itesm.mx>
2022-10-19 10:57:45 -0500

    Fix Layout

--- lib/widgets/organisms/LoginPage.dart
+++ lib/widgets/organisms/LoginPage.dart
@@ -21,78 +21,79 @@ class _LoginPageState extends State<StatefulWidget> {
         child: SafeArea(
             // Container 2
             child: Container(
-                // EdgeInserts - An immutable set of offsets in each of the four cardinal directions.
-                margin: const EdgeInsets.all(24),
-                // Columna
-                child: (Column(
-                  // Alineacióm
-                  crossAxisAlignment: CrossAxisAlignment.start,
-
-                  // Contenido en Pantalla
-                  children: const <Widget>[
-                    // Header
-                    AppTypography(
-                      align: TextAlign.center,
-                      type: "h1",
-                      text: "Iniciar Sesión",
-                      color: app_colors.primary,
-                    ),
-                    Divider(
-                      color: Color(0xFFFFFF),
-                      height: 32,
-                    ),
-
-                    //Correo
-                    AppTypography(
-                      align: TextAlign.left,
-                      type: "body2",
-                      text: "Correo:",
-                      color: app_colors.primary,
-                    ),
-                    Divider(
-                      color: Color(0xFFFFFF),
-                      height: 8,
-                    ),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Correo',
-                      ),
-                    ),
-                    Divider(
-                      color: Color(0xFFFFFF),
-                      height: 32,
-                    ),
-
-                    // Password
-                    AppTypography(
-                      align: TextAlign.left,
-                      type: "body2",
-                      text: "Contraseña:",
-                      color: app_colors.primary,
-                    ),
-                    Divider(
-                      color: Color(0xFFFFFF),
-                      height: 8,
-                    ),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: 'Contraseña',
-                      ),
-                    ),
-                    Divider(
-                      color: Color(0xFFFFFF),
-                      height: 32,
-                    ),
-
-                    // Boton - Iniciar sersion
-                    //AppButton(text: "Iniciar sesióm", onPressed: ()=>{}),
-                  ],
-                )))),
+          // EdgeInserts - An immutable set of offsets in each of the four cardinal directions.
+          margin: const EdgeInsets.all(24),
+          // Columna
+          child: (Column(
+            // Alineacióm
+            crossAxisAlignment: CrossAxisAlignment.center,
+            // Contenido en Pantalla
+            children: [
+              // Header
+              const AppTypography(
+                align: TextAlign.center,
+                type: "h1",
+                text: "Iniciar Sesión",
+                color: app_colors.primary,
+              ),
+              const SizedBox(
+                height: 33,
+              ),
+              _inputForm(),
+              // Boton - Iniciar sersion
+              //AppButton(text: "Iniciar sesióm", onPressed: ()=>{}),
+            ],
+          )),
+        )),
       ),
     );
   }
+}
+
+_inputForm() {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: const [
+      AppTypography(
+        align: TextAlign.left,
+        type: "body2",
+        text: "Correo",
+        color: app_colors.primary,
+      ),
+      SizedBox(
+        height: 8,
+      ),
+      TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Correo',
+        ),
+      ),
+      SizedBox(
+        height: 32,
+      ),
+
+      // Password
+      AppTypography(
+        align: TextAlign.left,
+        type: "body2",
+        text: "Contraseña:",
+        color: app_colors.primary,
+      ),
+      SizedBox(
+        height: 8,
+      ),
+      TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Contraseña',
+        ),
+      ),
+      SizedBox(
+        height: 32,
+      ),
+    ],
+  );
 }
