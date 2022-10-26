5cd19a0bb8eb3f53d2c383b0fbc5acab7ed35795
Jesus MJ <mj.jesusj@gmail.com>
2022-10-26 10:34:24 -0500

    Revert "F/login logic"

--- lib/widgets/molecules/inputLogin.dart
+++ lib/widgets/molecules/inputLogin.dart
@@ -1,43 +1,12 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 
 import 'package:app/styles/colors.dart' as app_colors;
 import 'package:app/consts/urls.dart' as app_urls;
 import 'package:app/widgets/atoms/Typography.dart';
 import 'package:app/widgets/atoms/Button.dart';
-import 'package:http/http.dart';
 
-class AppInputLoginForm extends StatefulWidget {
+class AppInputLoginForm extends StatelessWidget {
   const AppInputLoginForm({super.key});
-
-  @override
-  _AppInputLoginFormState createState() => _AppInputLoginFormState();
-}
-
-class _AppInputLoginFormState extends State<AppInputLoginForm> {
-  TextEditingController emailController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  void login(String email, password) async {
-    print(email + " " + password);
-    try {
-      Response response = await post(
-          Uri.parse('https://reqres.in/api/register'),
-          body: {'email': email, 'password': password});
-
-      if (response.statusCode == 200) {
-        var data = jsonDecode(response.body.toString());
-
-        print(data['token']);
-        print('Login successfully');
-      } else {
-        print(response.statusCode);
-        print('failed');
-      }
-    } catch (e) {
-      print(e.toString());
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,11 +22,9 @@ class _AppInputLoginFormState extends State<AppInputLoginForm> {
         const SizedBox(
           height: 8,
         ),
-        TextFormField(
+        const TextField(
           obscureText: false,
-          controller: emailController,
-          decoration: const InputDecoration(
-            hintText: 'Correo',
+          decoration: InputDecoration(
             border: OutlineInputBorder(),
           ),
         ),
@@ -75,35 +42,19 @@ class _AppInputLoginFormState extends State<AppInputLoginForm> {
         const SizedBox(
           height: 8,
         ),
-        TextFormField(
+        const TextField(
           obscureText: true,
-          controller: passwordController,
-          decoration: const InputDecoration(
-            hintText: 'Contraseña',
+          decoration: InputDecoration(
             border: OutlineInputBorder(),
           ),
         ),
         const SizedBox(
           height: 32,
         ),
-        GestureDetector(
-          onTap: () {
-            login(
-              emailController.text.toString(),
-              passwordController.text.toString(),
-            );
-          },
-          child: Container(
-            height: 50,
-            decoration: const BoxDecoration(color: app_colors.primary),
-            child: Center(
-              child: AppButton(
-                text: "Iniciar Sesión",
-                onPressed: () => {},
-              ),
-            ),
-            //text: "Iniciar Sesión",
-            //onPressed: () => Navigator.pushNamed(context, app_urls.home),
+        Center(
+          child: AppButton(
+            text: "Iniciar Sesión",
+            onPressed: () => Navigator.pushNamed(context, app_urls.home),
           ),
         ),
         const SizedBox(
