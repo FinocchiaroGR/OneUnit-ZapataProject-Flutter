215ae17a6ff9d3d58ad0ce45053674d54c4568a0
Giovanni Finocchiaro <A01704007@tec.mx>
2022-11-15 19:46:21 -0600

    Consult Car Info donde + Better provider management + Dispose all important data when loggin out

--- lib/providers/UserProvider.dart
+++ lib/providers/UserProvider.dart
@@ -1,8 +1,19 @@
+import 'dart:collection';
+import 'dart:convert';
+
+import 'package:app/models/CarModel.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class UserProvider extends ChangeNotifier {
-  String? _id;
-  String? _token;
+  late String _id;
+  late String _token;
+  late List<CarModel> _cars = [];
+
+  String get id => _id;
+  String get token => _token;
+
+  UnmodifiableListView<CarModel> get cars => UnmodifiableListView(_cars);
 
   void signIn(String id, String token) {
     _id = id;
@@ -10,11 +21,21 @@ class UserProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  void saveCars(String id, String token, Response response) {
+    var responseBody = jsonDecode(response.body);
+    for (int i = 0; i < responseBody.length; i++) {
+      _cars!.add(CarModel.fromJson(responseBody[i]));
+      notifyListeners();
+    }
+  }
+
   void deleteAll() {
-    _id = null;
-    _token = null;
+    _id = "";
+    _token = "";
+    _cars.clear();
   }
 
   String? getID() => _id;
   String? getToken() => _token;
+  List<CarModel>? getCars() => _cars;
 }
