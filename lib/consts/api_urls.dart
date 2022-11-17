a009ee70d2a105cc03fe16ca491578cf55660312
Giovanni Finocchiaro <A01704007@tec.mx>
2022-11-17 12:51:38 -0600

    Fixing minors problems

--- lib/consts/api_urls.dart
+++ lib/consts/api_urls.dart
@@ -5,7 +5,7 @@ For ios emulation use:
 const baseurl = "http://localhost:1337/api";
 */
 const baseurl = "http://localhost:1337/api";
-Uri user = Uri.parse("$baseurl/clients/flutter/");
+Uri user = Uri.parse("$baseurl/clients/");
 Uri login = Uri.parse("$baseurl/auth/login");
 Uri logout = Uri.parse("$baseurl/auth/logout");
 Uri cars = Uri.parse("$baseurl/cars/client/");
