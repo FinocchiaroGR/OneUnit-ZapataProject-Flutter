d2f24cafb7118526eaa0d9a1c15ba3cbc9958389
“skereboi” <mj.jesusj@gmail.com>
2022-12-04 20:44:56 -0600

    Correcion de Page FAQ, primera parte del realease, app logo

--- lib/pages/FAQs.dart
+++ lib/pages/FAQs.dart
@@ -22,65 +22,94 @@ class FAQs extends StatelessWidget {
           navigationCurrentIndex: 2,
           body: SingleChildScrollView(
             child: Column(
-				crossAxisAlignment: CrossAxisAlignment.start,
-				children: <Widget>[
-					const AppTypography(
-					align: TextAlign.center,
-					type: "h1",
-					text: "Preguntas Frecuentes",
-					color: app_colors.primary,
-				),
-				const AppTypography(
-					align: TextAlign.left,
-					type: "h3",
-					text: "¿Cuál es la diferencia horaria entre Qatar, Argentina/México/España/Sudamérica?",
-					color: app_colors.primary,
-				),
-				const AppTypography(
-					align: TextAlign.left,
-					type: "body2",
-					text: "Qatar se encuentra dentro del huso horario UTC/GMT +3 horas. Para tenerlo en cuenta en otros lugares del planeta a la hora de saber los horarios de cada partido. Por ejemplo, en Argentina serían seis horas menos como buena parte de Sudamérica, en España dos horas y en México ocho horas menos.",
-					color: app_colors.primary,
-				),
-					const AppTypography(
-					align: TextAlign.left,
-					type: "h3",
-					text: "¿Cuánto gana el campeón del Mundial de Qatar 2022?",
-					color: app_colors.primary,
-				),
-				const AppTypography(
-					align: TextAlign.left,
-					type: "body2",
-					text: "La FIFA ha dispuesto una cuantía global en premios de 440 millones de dólares. De los cuáles, 42 millones de dólares irán destinados a la selección vencedora, y 30 millones al subcampeón.",
-					color: app_colors.primary,
-				),
-					const AppTypography(
-					align: TextAlign.left,
-					type: "h3",
-					text: "¿Qué jugadores jugarán su último Mundial en 2022?",
-					color: app_colors.primary,
-				),
-				const AppTypography(
-					align: TextAlign.left,
-					type: "body2",
-					text: "Es difícil pronosticar que jugadores de primer orden mundial disputarán su última cita de esta entidad. Por edad, es previsible que jugadores que han marcado los últimos años del fútbol al más alto nivel como Cristiano Ronaldo, Leo Messi, Diego Godín o Sergio Busquets se encuentren en esa tesitura.",
-					color: app_colors.primary,
-				),
-					const AppTypography(
-					align: TextAlign.left,
-					type: "h3",
-					text: "¿Qué selecciones africanas clasificaron para Qatar 2022?",
-					color: app_colors.primary,
-				),
-				const AppTypography(
-					align: TextAlign.left,
-					type: "body2",
-					text: "En esta ocasión, Camerún, Ghana, Marruecos, Senegal y Túnez son las cinco naciones encargadas de representar al continente africano en el Mundial de Qatar 2022.",
-					color: app_colors.primary,
-				),
-			],
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const AppTypography(
+                  align: TextAlign.center,
+                  type: "h1",
+                  text: "Preguntas Frecuentes",
+                  color: app_colors.primary,
+                ),
+                const AppTypography(
+                  align: TextAlign.left,
+                  type: "body1",
+                  text:
+                      "La App de Grupo Zapata que vincula tu smartphone con tu auto para darte:",
+                  color: app_colors.primary,
+                ),
+                const SizedBox(height: 40),
+                const AppTypography(
+                  align: TextAlign.left,
+                  type: "subtitle",
+                  text: "Ubicación en Tiempo Real",
+                  color: app_colors.primary,
+                ),
+                const AppTypography(
+                  align: TextAlign.left,
+                  type: "body2",
+                  text:
+                      "¡Crea una Geocerca y recibe alertas en caso de que el auto salga de la zona delimitada!",
+                  color: app_colors.primary,
+                ),
+                const SizedBox(height: 30),
+                const AppTypography(
+                  align: TextAlign.left,
+                  type: "subtitle",
+                  text: "Velocímetro en Tiempo Real",
+                  color: app_colors.primary,
+                ),
+                const AppTypography(
+                  align: TextAlign.left,
+                  type: "body2",
+                  text:
+                      "¡Recibe alertas en caso de que tu auto sobrepase la velocidad que decidas establecer en los parámetros!",
+                  color: app_colors.primary,
+                ),
+                const SizedBox(height: 30),
+                const AppTypography(
+                  align: TextAlign.left,
+                  type: "subtitle",
+                  text: "Alerta de Movimiento",
+                  color: app_colors.primary,
+                ),
+                const AppTypography(
+                  align: TextAlign.left,
+                  type: "body2",
+                  text:
+                      "Enciende el modo 'parking' para que te alerte si tu auto.se esta moviendo",
+                  color: app_colors.primary,
+                ),
+                const SizedBox(height: 30),
+                const AppTypography(
+                  align: TextAlign.left,
+                  type: "subtitle",
+                  text: "Concentra toda la información",
+                  color: app_colors.primary,
+                ),
+                const AppTypography(
+                  align: TextAlign.left,
+                  type: "body2",
+                  text:
+                      "Recibe alertas acerca de la vigencia de toda tú documentación relacionada con tu auto.",
+                  color: app_colors.primary,
+                ),
+                const SizedBox(height: 30),
+                const AppTypography(
+                  align: TextAlign.left,
+                  type: "subtitle",
+                  text: "Mantente siempre en contacto con Zapata",
+                  color: app_colors.primary,
+                ),
+                const AppTypography(
+                  align: TextAlign.start,
+                  type: "body2",
+                  text:
+                      "Agenda tu cita de servicio y recibe ofertas personalizadas.",
+                  color: app_colors.primary,
+                ),
+              ],
+            ),
           ),
-		  ),
         );
       },
     );
